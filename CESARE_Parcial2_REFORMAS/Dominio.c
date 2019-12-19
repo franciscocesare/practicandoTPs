2c56a1d57572c655feb3a5d7be702240a4ad9c62
franciscocesare <pako_cesare@hotmail.com>
2019-12-19 00:07:49 -0300

    cassaaaa

--- CESARE_Parcial2_REFORMAS/Dominio.c
+++ CESARE_Parcial2_REFORMAS/Dominio.c
@@ -11,12 +11,12 @@
  * \param Toma el valor ID ingresado por el usuario.
  * \return Retorna 0 (cero) o 1 (uno).
  */
-int eCachorro_setId(eCachorro* listaCachorro,int id)
+int eComputer_setId(eComputer* listaComputer,int id)
 {
     int result=0;
-    if(listaCachorro!=NULL && id >0)
+    if(listaComputer!=NULL && id >0)
     {
-        listaCachorro->id_cachorro = id;
+        listaComputer->id = id;
         result=1;
     }
 
@@ -29,13 +29,13 @@ int eCachorro_setId(eCachorro* listaCachorro,int id)
  * \param puntero a entero y que guardara el valor del ID.
  * \return Retorna 0 (cero) o 1 (uno).
  */
-int eCachorro_getId(eCachorro* listaCachorro,int* id)
+int eComputer_getId(eComputer* listaComputer,int* id)
 {
     int result=0;
 
-    if(listaCachorro!=NULL && id!=NULL)
+    if(listaComputer!=NULL && id!=NULL)
     {
-        *id = listaCachorro->id_cachorro;
+        *id = listaComputer->id;
         result = 1;
     }
 
@@ -48,12 +48,12 @@ int eCachorro_getId(eCachorro* listaCachorro,int* id)
  * \param Recibe cadena de caracteres.
  * \return Retorna 0 (cero) o 1 (uno).
  */
-int eCachorro_setNombre(eCachorro* listaCachorro, char* nombre)
+int eComputer_setDescripcion(eComputer* listaComputer, char* descripcion)
 {
     int result=0;
-    if( listaCachorro != NULL && nombre != NULL && strlen(nombre) <= 20 )
+    if( listaComputer != NULL && descripcion != NULL && strlen(descripcion) <= 150 )
     {
-        strcpy(listaCachorro->nombre, nombre);
+        strcpy(listaComputer->descripcion, descripcion);
         result = 1;
     }
     return result;
@@ -66,13 +66,13 @@ int eCachorro_setNombre(eCachorro* listaCachorro, char* nombre)
  * \param Puntero a char donde guardara la cadena de la estructura.
  * \return Retorna 0 (cero) o 1 (uno).
  */
-int eCachorro_getNombre(eCachorro* listaCachorro,char* nombre)
+int eComputer_getDescripcion(eComputer* listaComputer,char* descripcion)
 {
 
     int result=0;
-    if( listaCachorro != NULL && nombre != NULL)
+    if( listaComputer != NULL && descripcion != NULL)
     {
-        strcpy( nombre,listaCachorro->nombre);
+        strcpy( descripcion,listaComputer->descripcion);
         result = 1;
     }
     return result;
@@ -84,13 +84,13 @@ int eCachorro_getNombre(eCachorro* listaCachorro,char* nombre)
  * \param Toma el valor Horas ingresado por el usuario.
  * \return Retorna 0 (cero) o 1 (uno).
  */
-int eCachorro_getDias(eCachorro* listaCachorro,int* dias)
+int eComputer_getPrecio(eComputer* listaComputer,float* precio) ///precio
 {
 
     int result=0;
-    if( listaCachorro != NULL && dias > 0)
+    if( listaComputer != NULL && precio!=NULL)
     {
-        *dias = listaCachorro->dias;
+        *precio = listaComputer->precio;
         result = 1;
     }
     return result;
@@ -103,14 +103,14 @@ int eCachorro_getDias(eCachorro* listaCachorro,int* dias)
  * \param Puntero a entero y que guardara el valor de variable HorasTrabajadas.
  * \return Retorna 0 (cero) o 1 (uno).
  */
-int eCachorro_setDias(eCachorro* listaCachorro,int dias)
+int eComputer_setPrecio(eComputer* listaComputer,float precio)
 {
 
     int result=0;
 
-    if(listaCachorro!=NULL && dias>0)
+    if(listaComputer!=NULL)///va el precio!=NULL?
     {
-        listaCachorro->dias = dias;
+        listaComputer->precio = precio;
         result = 1;
     }
     return result;
@@ -122,14 +122,15 @@ int eCachorro_setDias(eCachorro* listaCachorro,int dias)
  * \param Recibe cadena de caracteres.
  * \return Retorna 0 (cero) o 1 (uno).
  */
-int eCachorro_setRaza(eCachorro* listaCachorro, char* raza)
+int eComputer_setIdTipo(eComputer* listaComputer,int idTipo)
 {
     int result=0;
-    if( listaCachorro != NULL && raza!= NULL && strlen(raza) <= 20 )
+    if(listaComputer!=NULL && idTipo >0)
     {
-        strcpy(listaCachorro->raza, raza);
-        result = 1;
+        listaComputer->idTipo = idTipo;
+        result=1;
     }
+
     return result;
 }
 
@@ -140,66 +141,33 @@ int eCachorro_setRaza(eCachorro* listaCachorro, char* raza)
  * \param Puntero a char donde guardara la cadena de la estructura.
  * \return Retorna 0 (cero) o 1 (uno).
  */
-int eCachorro_getRaza(eCachorro* listaCachorro,char* raza)
+int eComputer_getIdTipo(eComputer* listaComputer,int* idTipo)
 {
-
     int result=0;
-    if( listaCachorro != NULL && raza != NULL)
+
+    if(listaComputer!=NULL && idTipo!=NULL)
     {
-        strcpy( raza,listaCachorro->raza);
+        *idTipo = listaComputer->idTipo;
         result = 1;
     }
+
     return result;
 }
-
 /** \brief Valida el valor sueldo y lo carga en estructura
  *
  * \param Recibe puntero de estructura Employee
  * \param Toma el valor Sueldo ingresado por el usuario
  * \return Retorna 0 (cero) o 1 (uno).
  */
-int eCachorro_setGenero(eCachorro* listaCachorro,char* genero)
+
+
+int eComputer_setOferta(eComputer* listaComputer,char* oferta)
 {
 
     int result=0;
-    if(listaCachorro!=NULL  )
+    if(listaComputer!=NULL  )
     {
-        listaCachorro->genero = *genero;
-        result=1;
-    }
-
-    return result;
-}
-
-/** \brief Toma valor de Sueldo de una estructura Employee.
- *
- * \param recibe puntero de estructura Employee.
- * \param Puntero a entero y que guardara el valor del sueldo.
- * \return Retorna 0 (cero) o 1 (uno).
- */
-int eCachorro_getGenero(eCachorro* listaCachorro,char* genero)
-{
-
-    int result=0;
-
-    if(listaCachorro!=NULL && genero != NULL)
-    {
-        *genero=listaCachorro->genero;
-        ///*genero = listaCachorro->genero;
-        result = 1;
-    }
-    return result;
-}
-
-///aca agrego la de reservado igual a genero
-
-int eCachorro_setReservado(eCachorro* listaCachorro,char* reservado)
-{
-
-    int result=0;
-    if(listaCachorro!=NULL  )
-    {
-        strcpy(listaCachorro->reservado, reservado);
+        strcpy(listaComputer->oferta, oferta);
         ///listaCachorro->reservado = *reservado;
         result=1;
     }
@@ -213,15 +181,14 @@ int eCachorro_setReservado(eCachorro* listaCachorro,char* reservado)
  * \param Puntero a entero y que guardara el valor del sueldo.
  * \return Retorna 0 (cero) o 1 (uno).
  */
-int eCachorro_getReservado(eCachorro* listaCachorro,char* reservado)
+int eComputer_getOferta(eComputer* listaComputer,char* oferta)
 {
 
     int result=0;
 
-    if(listaCachorro!=NULL && reservado != NULL)
+    if(listaComputer!=NULL && oferta != NULL)
     {
-        strcpy(reservado, listaCachorro->reservado);
-        /// *reservado = listaCachorro->reservado;
+        strcpy(oferta, listaComputer->oferta);
         result = 1;
     }
     return result;
@@ -231,7 +198,7 @@ int eCachorro_getReservado(eCachorro* listaCachorro,char* reservado)
  *
  * \return Devuelve la nueva estructura creada con los campos inicializados
  */
-eCachorro* newCachorro()
+/*eCachorro* newCachorro()
 {
     eCachorro* nuevo = (eCachorro*) malloc(sizeof(eCachorro));
     if(nuevo != NULL)
@@ -242,6 +209,20 @@ eCachorro* newCachorro()
         strcpy(nuevo->raza, "");
         strcpy(nuevo->reservado, "");
         nuevo->genero=' ';
+
+    }
+    return nuevo;
+}*/
+eComputer* newComputer()
+{
+    eComputer* nuevo = (eComputer*) malloc(sizeof(eComputer));///nuevo->para los Int Float, strcpy para los string
+    if(nuevo != NULL)
+    {
+        nuevo->id= 0;
+        strcpy(nuevo->descripcion, "");
+        nuevo->precio = 0; ///precio float
+        nuevo->idTipo=0;///idTipo int
+        strcpy(nuevo->oferta, "");///Oferta char
 
     }
     return nuevo;
@@ -257,7 +238,29 @@ eCachorro* newCachorro()
  *
  * \return Retorna la estructura Employee* con los datos cargados en sus campos
  */
-eCachorro* newCachorro_Parametros(char* id,char* nombre,char* dias, char* raza, char* reservado,char* genero)
+
+ eComputer* newComputer_Parametros(char* id, char* descripcion, char* precio, char* idTipo, char* oferta)
+{
+    eComputer* nuevo = newComputer();
+    if( nuevo != NULL)
+    {
+        if (id!=NULL && descripcion!=NULL && precio!=NULL &&  idTipo!=NULL && oferta != NULL)
+        {
+            eComputer_setId(nuevo,atoi(id));
+            eComputer_setDescripcion(nuevo, descripcion);
+            eComputer_setPrecio(nuevo, atof(precio));  ///siempre en el New algo van los setters
+            eComputer_setIdTipo(nuevo,atoi(idTipo));
+            eComputer_setDescripcion(nuevo, descripcion);
+            eComputer_setOferta(nuevo, oferta);
+        }
+        else
+        {
+            free(nuevo);
+        }
+    }
+    return nuevo;
+}
+/*eCachorro* newCachorro_Parametros(char* id,char* nombre,char* dias, char* raza, char* reservado,char* genero)
 {
     eCachorro* nuevo = newCachorro();
     if( nuevo != NULL)
@@ -277,7 +280,7 @@ eCachorro* newCachorro_Parametros(char* id,char* nombre,char* dias, char* raza, 
         }
     }
     return nuevo;
-}
+}*/
 
 
 
@@ -287,28 +290,31 @@ eCachorro* newCachorro_Parametros(char* id,char* nombre,char* dias, char* raza, 
  *
  * \param Recibe LinkedList*
  */
-void showCachorro(eCachorro* nuevo)
+/*void showCachorro(eCachorro* nuevo)
 {
     if (nuevo!= NULL)
     {
         printf("%4d   %10s  %5d   %10s   %10s   %5c\n",nuevo->id_cachorro, nuevo->nombre, nuevo->dias, nuevo->raza, nuevo->reservado, nuevo->genero);
     }
-}
-/*void showDominio(eCachorro* dominios)
+}*/
+
+void showComputer(eComputer* compu)
 {
-    if(dominios!=NULL)
+    char descTipo [30]; ///para poner el tipo en vez de Id Tipo
+    if(compu!=NULL)
     {
-        printf("%4d  %10s  %5d    %5c\n",dominios->id,dominios->dominio,dominios->anio,dominios->tipo);
+        cargarDescripcion(compu, descTipo);
+        printf("%2d %10.2f  %12s  %-103s %12s\n",compu->id,compu->precio,descTipo, compu->descripcion,compu->oferta);
     }
 }
-*/
+
 /** \brief Muestra los datos de un empleado
  *
  * \param Recibe un puntero a Employee
  */
 
 ///void showDominios(LinkedList* pArrayLinkedDominio)
-void showCachorros(LinkedList* pArrayLinkedCachorro)
+/*void showCachorros(LinkedList* pArrayLinkedCachorro)
 {
     int tam;
 
@@ -328,11 +334,116 @@ void showCachorros(LinkedList* pArrayLinkedCachorro)
             showCachorro((eCachorro*) ll_get(pArrayLinkedCachorro, i));
         }
     }
+}*/
+void showComputers(LinkedList* pArrayLinkedComputers)
+{
+    int tam;
+system("cls");
+    printf(" ID   PRECIO         TIPO           DESCRIPCION                                                                                         OFERTA\n");
+    printf("--------------------------------------------------------------------------------------------------------------------------------------------------\n");
+
+    if(ll_len(pArrayLinkedComputers) == 0)
+    {
+        printf("NO HAY COMPUTADORAS QUE MOSTRAR\n\n");
+    }
+
+    if(pArrayLinkedComputers != NULL)
+    {
+        tam = ll_len(pArrayLinkedComputers);
+        for(int i=0; i<tam; i++)
+        {
+            showComputer((eComputer*) ll_get(pArrayLinkedComputers, i));
+        }
+    }
+}
+
+int cargarDescripcion(void* lista, char* desc)
+{
+    int todoOk = 0;
+    int idTipo;
+
+    if(lista != NULL && desc != NULL){
+        if(eComputer_getIdTipo(lista, &idTipo)){
+            if(idTipo == 1){
+                strcpy(desc, "DESKTOP");
+            }else if(idTipo == 2){
+                strcpy(desc, "LAPTOP");
+            }
+            todoOk=1;
+        }
+    }
+    return todoOk;
 }
 
 
+int sortComputers (void* desktop, void* laptop)///(void* Employee1, void* Employee2)
+{
+
+        int retorno;
+        int idTipoDesktop;
+        int idTipoLaptop;
+
+        eComputer* auxComputer1 = (eComputer*) desktop;
+        eComputer* auxComputer2 = (eComputer*) laptop;
+
+        eComputer_getIdTipo(auxComputer1, &idTipoDesktop);
+        eComputer_getIdTipo(auxComputer2, &idTipoLaptop);
+
+       if (idTipoDesktop > idTipoLaptop)
+        {
+            retorno = 1;
+        }
+     else if (idTipoDesktop < idTipoLaptop)
+
+        {
+            retorno=-1;
+        }
+        else{
+            retorno =0;
+        }
+
+    return retorno;
+}
+
+
+
+int filter_desktop(void* aux) ///la funcion que despues es pasada como pFUNC a ll_filter con filtro de CARACTERES
+{
+    int todoOk = 0;
+    int idTipo;
+
+    ///eCachorro* cachorro45 =NULL;
+    if(aux != NULL)
+    {
+
+        aux = (eComputer*)aux;
+        eComputer_getIdTipo(aux, &idTipo);
+        if (idTipo==2)
+            todoOk=1;
+    }
+    return todoOk;
+}
+
+int setearOferta(void* element)
+{
+    int result = 0;
+    eComputer* compu = NULL;
+
+    if( element != NULL){
+        compu = (eComputer*)element;
+        if( compu->idTipo == 2){
+            strcpy(compu->oferta, "SIN DATOS");
+        }
+        else if(compu->idTipo == 1   && compu->precio > 20000 ){
+            strcpy(compu->oferta, "50% DESCUENTO");
+        }
+        result = 1;
+    }
+    return result;
+}
+
 /// PARA FILTER
-int filter_45dias(void* aux) ///la funcion que despues es pasada como pFUNC a ll_filter filtro de INT
+/*int filter_45dias(void* aux) ///la funcion que despues es pasada como pFUNC a ll_filter filtro de INT
 {
     int todoOk = 0;
     int dias;
@@ -345,9 +456,11 @@ int filter_45dias(void* aux) ///la funcion que despues es pasada como pFUNC a ll
             todoOk=1;
     }
     return todoOk;
-}
+}*/
+
+
 ///FILTER SIN LOS CACHORROS QUE SON MACHOS
-int filter_machos(void* aux) ///la funcion que despues es pasada como pFUNC a ll_filter con filtro de UN CARACTER
+/*int filter_machos(void* aux) ///la funcion que despues es pasada como pFUNC a ll_filter con filtro de UN CARACTER
 {
     int todoOk = 0;
     char genero;
@@ -360,9 +473,10 @@ int filter_machos(void* aux) ///la funcion que despues es pasada como pFUNC a ll
             todoOk=1;
     }
     return todoOk;
-}
+}*/
 
-int filter_callejeros(void* aux) ///la funcion que despues es pasada como pFUNC a ll_filter con filtro de CARACTERES
+
+/*int filter_callejeros(void* aux) ///la funcion que despues es pasada como pFUNC a ll_filter con filtro de CARACTERES
 {
     int todoOk = 0;
     char raza [20];
@@ -377,11 +491,11 @@ int filter_callejeros(void* aux) ///la funcion que despues es pasada como pFUNC 
             todoOk=1;
     }
     return todoOk;
-}
+}*/
 
 ///FILTER DE RESERVADO COMO EJEMPLO
 
-int filter_reservado(void* aux) ///la funcion que despues es pasada como pFUNC a ll_filter con filtro de CARACTERES
+/*int filter_reservado(void* aux) ///la funcion que despues es pasada como pFUNC a ll_filter con filtro de CARACTERES
 {
     int todoOk = 0;
     char reservado [20];
@@ -395,7 +509,7 @@ int filter_reservado(void* aux) ///la funcion que despues es pasada como pFUNC a
             todoOk=1;
     }
     return todoOk;
-}
+}*/
 
 ///FILTER DE RO
 /*int employee_filterUnder45(void* aux)
